764ad833e8ab98181fa85471214c9fb9c6825ece
sertman1 <sertman1@jhu.edu>
2024-05-24 12:35:07 -0400

    successfully populating db

--- backend/server/data/db.js
+++ backend/server/data/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 async function connect() {
   try {
-    await mongoose.connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+    await mongoose.connect(process.env.DB_URI);
     console.log("Connected to MongoDB")
   } catch (err) {
     console.log(err);
